72a7c5cb016deabce6fc8d2d451feffba3b65cf9
Florian BENNETOT <fbenneto@e2r1p4.42.fr>
2017-11-21 14:57:27 +0100

    begin work on get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 14:49:35 by fbenneto          #+#    #+#             */
-/*   Updated: 2017/11/21 14:50:16 by fbenneto         ###   ########.fr       */
+/*   Updated: 2017/11/21 14:57:07 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int		get_next_line(const int fd, char **line)
 {
+	char	*buff;
+	int		i;
+	int		st;
 
+	if (!(buff = (char*)malloc(BUFF_SIZE * sizeof(char))))
+		return (-1);
+	while ((res = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+
+	}
+	free(res);
+	return ((res > 0) ? 1 : res);
 }
